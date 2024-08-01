68bb129099c4bc2c5b8875321a4fcd156b9e7f36
Jorge Matos <jisapjisap@gmail.com>
2024-08-01 19:33:01 +0200

    motion nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { motion } from "framer-motion"
+
+
+
+const heightAnimation = {
+  initial: {
+    height: 0
+  },
+  open: {
+    height: "85vh",
+    transition: {duration:1, ease: [0.75, 0, 0.23, 1]},
+  },
+  close: {
+    height: 0,
+    transition: {duration: 1, ease:[0.75, 0, 0.23, 1]}
+  }
+}
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <motion.div
+      variants={heightAnimation}
+      initial="initial"
+      animate="open"
+      exit="close"
+      className="bg-primary overflow-hidden absolute z-[50] -top-full w-full"
+    >
+      Nav
+    </motion.div>
   )
 }
 
