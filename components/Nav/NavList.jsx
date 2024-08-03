5bd18e292ecc5de3d1e3d29f4bddcd720a9a6e11
Jorge Matos <jisapjisap@gmail.com>
2024-08-03 17:21:57 +0200

    comments

--- components/Nav/NavList.jsx
+++ components/Nav/NavList.jsx
@@ -11,9 +11,9 @@ const links = [
   { href: "/", name: "Contact" },
 ];
 
-const getLetter = (name) => {
+const getLetter = (name) => {                               // getLetter recibe un nombre (name), 
   let letters = [];
-  name.split("").forEach((letter, index) => {
+  name.split("").forEach((letter, index) => {               // lo divide en letras, y devuelve un array de elementos motion.span con animaciones.
     letters.push(
       <motion.span 
         key={index}
@@ -21,7 +21,10 @@ const getLetter = (name) => {
         initial="initial"
         animate="enter"
         exit="exit"
-        custom={[index * 0.1, (name.length - index) * 0.05]} // propiedad personalizada que define los retrasos para las animaciones de entrada y salida.
+        custom={[                                           // Propiedad personalizada que define los retrasos para las animaciones de entrada y salida.
+          index * 0.1,                                      // retraso para la animación de entrada
+          (name.length - index) * 0.05                      // retraso para la animación de salida.
+        ]} 
       >
         {letter}
       </motion.span>
@@ -30,27 +33,28 @@ const getLetter = (name) => {
   return letters;
 }
 
-const letterAnim = {
-  initial: {
-    y: "150%",
-    opacity: 0,
+const letterAnim = {                                          // Define las animaciones para los estados initial, enter y exit de las letras.
+  
+  initial: {                                                  // Estado inicial        
+    y: "150%",                                                // (fuera de la vista hacia abajo)
+    opacity: 0,                                               // (transparente)
   },
-  enter: (i) => ({
-    y: 0,
+  enter: (i) => ({                                            // Cuando comienza la animación se toma el array custom
+    y: 0,                                                     // Se muestra la letra
     opacity: 1,
-    transition:{
+    transition:{                                              // Con su transición
       duration: 1,
       ease: [0.60,0, 0.20, 1],
-      delay: i[0]
+      delay: i[0]                                             // y su retraso (primer valor del array custom)
     },
   }),
-  exit: (i) => ({
-    y: "150%",
+  exit: (i) => ({                                             // Cuando termina la animación
+    y: "150%",                                                // se vuelve a ocultar
     opacity: 0,
-    transition: {
+    transition: {                                             // usando su transición
       duration: 0.8,
       ease: [0.60, 0, 0.20, 1],
-      delay: i[1]
+      delay: i[1]                                             // y su retrado (segundo valor del array custom)
     },
   }),
 }
