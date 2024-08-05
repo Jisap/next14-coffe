4182b6fe42b623707ff9126e4510756cfc01d34a
Jorge Matos <jisapjisap@gmail.com>
2024-08-05 19:21:28 +0200

    Menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,3 +1,4 @@
+import MenuItem from "./MenuItem"
 import Separator from "./Separator"
 
 
@@ -66,8 +67,35 @@ const menuItems = [
 
 const Menu = () => {
   return (
-    <section>
+    <section className="pt-12 pb-16 xl:pt-16 xl:pb-36">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-4 mb-12 xl:mb-24">
+          <h2 className="h2 text-center">
+            Our Menu
+          </h2>
+          <div className="mb-4">
+            <Separator bg="accent"/>
+          </div>
+          <p className="text-center max-w-[620px] mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit placerat id mus dapibus nulla, morbi hendrerit mollis dui cursus blandit iaculis 
+            sollicitudin sapien sagittis. Primis nulla bibendum quisque interdum cum vulputate
+          </p>
+        </div>
 
+        <div className="flex flex-col items-center gap-12 xl:gap-24">
+        {/* menu grid */}
+          <div className="w-full grid grid-cols-1 xl:grid-cols-2 gap-y-8 gap-x-16 place-content-center"> 
+            {menuItems.map((item, index) => {
+              return (
+                <div key={index}>
+                  <MenuItem />
+                </div>
+              )
+            })}
+          </div>
+          <button className="btn">View full menu</button>
+        </div>
+      </div>
     </section>
   )
 }
